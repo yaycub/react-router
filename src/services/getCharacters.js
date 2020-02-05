a5efa7bd8d281dcbea387a9e5dddda4ac6b61c3a
Jacob Harrington <jharrington4@gmail.com>
2020-02-05 14:04:39 -0800

    getCharacters service completed

---
+++ src/services/getCharacters.js
@@ -0,0 +1,17 @@
+export const getCharacters = (page) => {
+  fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+    .then(res => res.json())
+    .then(({ results }) => {
+      return results.map(character => {
+        return {
+          id: character.id,
+          name: character.name,
+          status: character.status,
+          species: character.species,
+          origin: character.origin.name,
+          location: character.location.name,
+          image: character.image
+        };
+      });
+    });
+};
