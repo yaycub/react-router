8a1cef1318602c6a27720115329be9e3f8062766
Jacob Harrington <jharrington4@gmail.com>
2020-02-05 16:25:48 -0800

    finished styling on Header component

--- src/components/rickandmorty/Header.js
+++ src/components/rickandmorty/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './styles/Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <header>
+  <header className={styles.Header}>
     <h1>React and Morty</h1>
     <nav>
-      <li><Link to="/">Home</Link></li>
+      <li><Link to="/" className={styles.Link}>Home</Link></li>
     </nav>
   </header>
 );
